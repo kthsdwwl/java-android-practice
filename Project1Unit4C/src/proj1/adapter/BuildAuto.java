de50742ddaa76f3afc96f5e40abe5d55a492bcf6
Lin <kthsdwwl@gmail.com>
2015-10-10 15:37:23 -0400

    removed unit4

--- Project1Unit4C/src/proj1/adapter/BuildAuto.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Xi Lin
- */
-
-package proj1.adapter;
-
-public class BuildAuto extends ProxyAuto implements CreateAuto, UpdateAuto, FixAuto, EditAndPrintAuto {
-
-}
--- Project1Unit4S/src/proj1/exception/ErrType.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Xi Lin
- */
-
-package proj1.exception;
-
-public enum ErrType {
-	NO_MODEL_NAME, NO_BASE_PRICE, NO_SET_NAME, NO_OPT_NAME, NO_OPT_PRICE
-}
